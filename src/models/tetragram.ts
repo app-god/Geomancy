9a8bc0922cc3b240c581e6831852aac40454ef55
adam <aneeley@gmail.com>
2017-04-09 15:42:44 -0700

    add descriptions to tetragrams

--- src/models/tetragram.ts
+++ src/models/tetragram.ts
@@ -2,6 +2,7 @@
 export class Tetragram {
 
   name: string
+  desc: string
 
   row0: number
   row1: number
@@ -9,8 +10,8 @@ export class Tetragram {
   row3: number
 
   constructor(public key: number = Math.floor(Math.random() * 15)) {
-    console.log(key)
     this.name = tetragramData[key].name
+    this.desc = tetragramData[key].desc
 
     this.row0 = (key >> 0) % 2
     this.row1 = (key >> 1) % 2
@@ -31,66 +32,82 @@ export class Tetragram {
 var tetragramData = [
   {
     key: 0,
-    name: 'Via'
+    name: 'Via',
+    desc: 'Injurious to the goodness of other figures generally, but good for journeys and voyages.'
   },
   {
     key: 1,
-    name: 'Caput Draconis'
+    name: 'Caput Draconis',
+    desc: 'Good with good, evil with evil. Gives good issue for gain.'
   },
   {
     key: 2,
-    name: 'Puella'
+    name: 'Puella',
+    desc: 'Good in all demands, especially in those things relating to women.'
   },
   {
     key: 3,
-    name: 'Fortuna Major'
+    name: 'Fortuna Major',
+    desc: 'Good for gain in all things where a person has hopes to win.'
   },
   {
     key: 4,
-    name: 'Puer'
+    name: 'Puer',
+    desc: 'Evil in most demands, except in those things relating to war or love.'
   },
   {
     key: 5,
-    name: 'Acquisitio'
+    name: 'Acquisitio',
+    desc: 'Generally good for profit and gain.'
   },
   {
     key: 6,
-    name: 'Carcer'
+    name: 'Carcer',
+    desc: 'Generally evil. Delay, binding, bar, restriction.'
   },
   {
     key: 7,
-    name: 'Tristitia'
+    name: 'Tristitia',
+    desc: 'Evil in almost all things.'
   },
   {
     key: 8,
-    name: 'Cauda Draconis'
+    name: 'Cauda Draconis',
+    desc: 'Good with evil, and evil with good. Good for loss, and for passing out of an affair.'
   },
   {
     key: 9,
-    name: 'Conjunctio'
+    name: 'Conjunctio',
+    desc: 'Good with good, or evil with evil. Recovery from things lost.'
   },
   {
     key: 10,
-    name: 'Amissio'
+    name: 'Amissio',
+    desc: 'Good for loss of substance and sometimes for love, but very bad for gain.'
   },
   {
     key: 11,
-    name: 'Albus'
+    name: 'Albus',
+    desc: 'Good for profit and for entering into a place or undertaking.'
   },
   {
     key: 12,
-    name: 'Fortuna Minor'
+    name: 'Fortuna Minor',
+    desc: 'Good in any manner in which a person wishes to proceed quickly.'
   },
   {
     key: 13,
-    name: 'Rubeus'
+    name: 'Rubeus',
+    desc: 'Evil in all that is good and good in all that is evil.'
   },
   {
     key: 14,
-    name: 'Laetitia'
+    name: 'Laetitia',
+    desc: 'Good for joy, present or to come.'
   },
   {
     key: 15,
-    name: 'Populus'
+    name: 'Populus',
+    desc: 'Sometimes good and sometimes bad; good with good, and evil with evil.'
   }
 ]
