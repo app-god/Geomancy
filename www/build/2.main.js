eef4f084cde0f29d55266962d1646d9e6b3bb528
adam <aneeley@gmail.com>
2017-04-09 17:31:07 -0700

    changes

--- www/build/2.main.js
+++ www/build/2.main.js
@@ -1,14 +1,14 @@
 webpackJsonp([2],{
 
-/***/ 318:
+/***/ 311:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(39);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__random__ = __webpack_require__(319);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "RandomPageModule", function() { return RandomPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__reading__ = __webpack_require__(312);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ReadingPageModule", function() { return ReadingPageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,26 +18,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var RandomPageModule = (function () {
-    function RandomPageModule() {
+var ReadingPageModule = (function () {
+    function ReadingPageModule() {
     }
-    return RandomPageModule;
+    return ReadingPageModule;
 }());
-RandomPageModule = __decorate([
+ReadingPageModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__random__["a" /* RandomPage */],
+            __WEBPACK_IMPORTED_MODULE_2__reading__["a" /* ReadingPage */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__random__["a" /* RandomPage */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__reading__["a" /* ReadingPage */]),
         ],
         exports: [
-            __WEBPACK_IMPORTED_MODULE_2__random__["a" /* RandomPage */]
+            __WEBPACK_IMPORTED_MODULE_2__reading__["a" /* ReadingPage */]
         ]
     })
-], RandomPageModule);
+], ReadingPageModule);
 
-//# sourceMappingURL=random.module.js.map
+//# sourceMappingURL=reading.module.js.map
 
 /***/ })
 
--- www/build/3.main.js
+++ www/build/3.main.js
@@ -1,14 +1,14 @@
 webpackJsonp([3],{
 
-/***/ 311:
+/***/ 318:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(39);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__reading__ = __webpack_require__(312);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "ReadingPageModule", function() { return ReadingPageModule; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__random__ = __webpack_require__(319);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "RandomPageModule", function() { return RandomPageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -18,26 +18,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 
 
-var ReadingPageModule = (function () {
-    function ReadingPageModule() {
+var RandomPageModule = (function () {
+    function RandomPageModule() {
     }
-    return ReadingPageModule;
+    return RandomPageModule;
 }());
-ReadingPageModule = __decorate([
+RandomPageModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__reading__["a" /* ReadingPage */],
+            __WEBPACK_IMPORTED_MODULE_2__random__["a" /* RandomPage */],
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__reading__["a" /* ReadingPage */]),
+            __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["d" /* IonicPageModule */].forChild(__WEBPACK_IMPORTED_MODULE_2__random__["a" /* RandomPage */]),
         ],
         exports: [
-            __WEBPACK_IMPORTED_MODULE_2__reading__["a" /* ReadingPage */]
+            __WEBPACK_IMPORTED_MODULE_2__random__["a" /* RandomPage */]
         ]
     })
-], ReadingPageModule);
+], RandomPageModule);
 
-//# sourceMappingURL=reading.module.js.map
+//# sourceMappingURL=random.module.js.map
 
 /***/ })
 
