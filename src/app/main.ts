813d78bd7723dbf9832a9cbb5802b271eeecbd1c
adam <aneeley@gmail.com>
2017-04-15 00:10:15 -0700

    console.log process env MODE

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core'
 import { AppModule } from './app.module';
 
+console.log(process.env.MODE)
+
 if (process.env.MODE == 'production') {
   enableProdMode()
 }
