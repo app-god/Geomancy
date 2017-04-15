47bef8d3cf78ccd6cf31c41c1835d08cce1a3ec2
adam <aneeley@gmail.com>
2017-04-15 00:52:38 -0700

    production mode

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,11 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core'
 import { AppModule } from './app.module';
 
-const envMode = process.env.MODE || 'development'
-console.log('environment: ', envMode)
-
-if (envMode == 'production') {
-  enableProdMode()
-}
+enableProdMode()
 
 platformBrowserDynamic().bootstrapModule(AppModule);
