b0641aba9cadaf248abf7234d1b2746a1e136295
adam <aneeley@gmail.com>
2017-04-18 19:20:38 -0700

    add warnings to reading-overview

--- src/pages/reading-overview/reading-overview.ts
+++ src/pages/reading-overview/reading-overview.ts
@@ -8,8 +8,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ReadingOverviewPage {
   reading
+  warnings = []
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.reading = navParams.data
+
+    let house1Tetragram = this.reading.getTetragramForHouse(1)
+    if (house1Tetragram.name == 'Rubeus'
+      || house1Tetragram.name == 'Cauda Draconis') {
+
+      let message = house1Tetragram.name + ' is in the first house.'
+      this.warnings.push(message)
+    }
   }
 }
--- www/build/1.main.js
+++ www/build/1.main.js
@@ -554,18 +554,26 @@ var ReadingOverviewPage = (function () {
     function ReadingOverviewPage(navCtrl, navParams) {
         this.navCtrl = navCtrl;
         this.navParams = navParams;
+        this.warnings = [];
         this.reading = navParams.data;
+        var house1Tetragram = this.reading.getTetragramForHouse(1);
+        if (house1Tetragram.name == 'Rubeus'
+            || house1Tetragram.name == 'Cauda Draconis') {
+            var message = house1Tetragram.name + ' is in the first house.';
+            this.warnings.push(message);
+        }
     }
     return ReadingOverviewPage;
 }());
 ReadingOverviewPage = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["e" /* IonicPage */])(),
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["L" /* Component */])({
-        selector: 'page-reading-overview',template:/*ion-inline-start:"/Users/adam/AppGod/geomancy/src/pages/reading-overview/reading-overview.html"*/'<!--\n  Generated template for the ReadingOverviewPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Overview</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n  <p>\n    question: {{reading.question}}\n  </p>\n\n  <p>\n    topic: {{reading.topic}}\n  </p>\n\n</ion-content>\n'/*ion-inline-end:"/Users/adam/AppGod/geomancy/src/pages/reading-overview/reading-overview.html"*/,
+        selector: 'page-reading-overview',template:/*ion-inline-start:"/Users/adam/AppGod/geomancy/src/pages/reading-overview/reading-overview.html"*/'<!--\n  Generated template for the ReadingOverviewPage page.\n\n  See http://ionicframework.com/docs/components/#navigation for more info on\n  Ionic pages and navigation.\n-->\n<ion-header>\n\n  <ion-navbar>\n    <ion-title>Overview</ion-title>\n  </ion-navbar>\n\n</ion-header>\n\n\n<ion-content padding>\n\n  <div class="reading-question">{{reading.question}}</div>\n  <div class="reading-topic">{{reading.topic}}</div>\n\n  <h3>Warning</h3>\n\n  <ion-card color="danger" *ngFor="let warning of warnings">\n    <ion-card-content>\n      {{warning}}\n    </ion-card-content>\n  </ion-card>\n\n</ion-content>\n'/*ion-inline-end:"/Users/adam/AppGod/geomancy/src/pages/reading-overview/reading-overview.html"*/,
     }),
-    __metadata("design:paramtypes", [__WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */], __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */]])
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["f" /* NavController */]) === "function" && _a || Object, typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1_ionic_angular__["g" /* NavParams */]) === "function" && _b || Object])
 ], ReadingOverviewPage);
 
+var _a, _b;
 //# sourceMappingURL=reading-overview.js.map
 
 /***/ })
