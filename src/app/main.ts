7dcab88261912ce75fa86793495925a89e2ee59c
adam <aneeley@gmail.com>
2017-05-03 21:40:27 -0700

    testing github->heroku deployment

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,6 +2,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { enableProdMode } from "@angular/core";
 
+console.log('github->heroku deployment works')
 console.log('IONIC_ENV', process.env.IONIC_ENV)
 
 if (process.env.IONIC_ENV == 'prod') {
