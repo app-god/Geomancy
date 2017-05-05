405c294041574971da1dada290d7eaafc6fcacb5
adam <aneeley@gmail.com>
2017-05-04 18:46:39 -0700

    revert changes

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: node web.js
-release: npm run build:www
--- src/app/main.ts
+++ src/app/main.ts
@@ -2,7 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import { enableProdMode } from "@angular/core";
 
-console.log('github->heroku deployment works')
 console.log('IONIC_ENV', process.env.IONIC_ENV)
 
 if (process.env.IONIC_ENV == 'prod') {
